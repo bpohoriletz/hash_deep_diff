c25817cda643f382f725107100947857ff9ace79
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-03-31 15:07:05 +0300

    Added handlers for simple cases

--- test/hash_deep_diff/test_comparison.rb
+++ test/hash_deep_diff/test_comparison.rb
@@ -5,9 +5,30 @@ require 'test_helper'
 describe HashDeepDiff::Comparison do
   describe '#diff' do
     it 'returns an empty hash if left quals right' do
-      diff = HashDeepDiff::Comparison.new({}, {}).diff
+      left = { a: :b }
+      right = { a: :b }
+
+      diff = HashDeepDiff::Comparison.new(left, right).diff
 
       assert_empty(diff)
+    end
+
+    it 'returns left if right is empty' do
+      left = { a: :b }
+      right = {}
+
+      diff = HashDeepDiff::Comparison.new(left, right).diff
+
+      assert_equal(left, diff)
+    end
+
+    it 'returns right if left is empty' do
+      left = {}
+      right = { c: :d }
+
+      diff = HashDeepDiff::Comparison.new(left, right).diff
+
+      assert_equal(right, diff)
     end
   end
 end
