9ba0c02fe1f2a541f0b5d69500c7b3d8d2b7f822
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-03-31 15:45:33 +0300

    Added handler for simple one level deep hashes

--- lib/hash_deep_diff/comparison.rb
+++ lib/hash_deep_diff/comparison.rb
@@ -7,11 +7,12 @@ module HashDeepDiff
     attr_reader :left, :right
 
     def diff
-      return {} if left == right  # this is rder-sensitive comparison
-      return left if right.empty?
-      return right if left.empty?
+      return [{}, {}, {}] if left == right  # this is order-sensitive comparison
+      return [left, {}, {}] if right.empty?
+      return [{}, {}, right] if left.empty?
+      return first_level_delta if one_level_deep?
 
-      return {}
+      return nil
     end
 
     private
@@ -19,6 +20,49 @@ module HashDeepDiff
     def initialize(left, right)
       @left = left.to_hash
       @right = right.to_hash
+    end
+
+    def first_level_delta
+      [
+        right_delta,
+        delta,
+        left_delta
+      ]
+    end
+
+    def right_delta
+      right_diff_keys.each_with_object({}) do |key, memo|
+        memo[key] = right[key]
+      end
+    end
+
+    def delta
+      common_keys.each_with_object({}) do |key, memo|
+        memo[key] = right[key] unless right[key] == left[key]
+      end
+    end
+
+    def left_delta
+      left_diff_keys.each_with_object({}) do |key, memo|
+        memo[key] = left[key]
+      end
+    end
+
+    def one_level_deep?
+      left.values.none? { |value| value.respond_to?(:to_ary) || value.respond_to?(:to_hash) } &&
+        right.values.none? { |value| value.respond_to?(:to_ary) || value.respond_to?(:to_hash) }
+    end
+
+    def common_keys
+      left.keys & right.keys
+    end
+
+    def right_diff_keys
+      right.keys - left.keys
+    end
+
+    def left_diff_keys
+      left.keys - right.keys
     end
   end
 end
