70d65703ee846833b7e4830e0f26146b56fef91f
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-04-06 22:15:26 +0300

    Introduced Delta::Right - representation of the missing elements

--- lib/hash_deep_diff.rb
+++ lib/hash_deep_diff.rb
@@ -2,9 +2,9 @@
 
 require 'hash_deep_diff/version'
 require 'hash_deep_diff/delta/left'
+require 'hash_deep_diff/delta/right'
 require 'hash_deep_diff/comparison'
 
 module HashDeepDiff
   class Error < StandardError; end
-  # Your code goes here...
 end
