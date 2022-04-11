b6cc68c626faf19d4780bcda46a8f63da4668437
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-04-11 18:42:02 +0300

    Added path to Delta::Left

--- test/hash_deep_diff/delta/test_left.rb
+++ test/hash_deep_diff/delta/test_left.rb
@@ -3,9 +3,15 @@
 require 'test_helper'
 
 describe HashDeepDiff::Delta::Left do
-  let(:subject) { HashDeepDiff::Delta::Left.new(delta: load_fixture('one_level/small')) }
-
   it 'is convertable to string' do
+    subject = HashDeepDiff::Delta::Left.new(delta: load_fixture('one_level/small'))
+
     assert_equal('+left[a] = b', subject.to_s)
+  end
+
+  it 'appends prefix to a report' do
+    subject = HashDeepDiff::Delta::Left.new(delta: load_fixture('one_level/small'), prefix: [:b])
+
+    assert_equal('+left[b][a] = b', subject.to_s)
   end
 end
