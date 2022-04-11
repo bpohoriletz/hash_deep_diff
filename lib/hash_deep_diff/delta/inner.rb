b44e0ee014b99f418e3828fc8e984fbc3a34c642
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-04-11 20:21:11 +0300

    Exposed path for Deltas to outer world

--- lib/hash_deep_diff/delta/inner.rb
+++ lib/hash_deep_diff/delta/inner.rb
@@ -12,8 +12,8 @@ module HashDeepDiff
 
       def to_str
         lines = <<~Q
-          -left#{path} = #{@delta.values.first[:left]}
-          +right#{path} = #{@delta.values.first[:right]}
+          -left#{diff_prefix} = #{@delta.values.first[:left]}
+          +right#{diff_prefix} = #{@delta.values.first[:right]}
         Q
         lines.strip
       end
