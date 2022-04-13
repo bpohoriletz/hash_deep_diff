2b30d9cbf2506ca4ba7880e146df985b9f22e63e
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-04-14 00:05:44 +0300

    [REFACTOR] Simplify Delta::Left#to_str

--- test/hash_deep_diff/delta/test_right.rb
+++ test/hash_deep_diff/delta/test_right.rb
@@ -17,4 +17,28 @@ describe HashDeepDiff::Delta::Right do
 
     assert_equal("-left[a][b] = b\n-left[a][c] = c", subject.to_s)
   end
+
+  it 'appends prefix to a report' do
+    subject = HashDeepDiff::Delta::Right.new(path: %i[b a], value: small[:a])
+
+    assert_equal('-left[b][a] = b', subject.to_s)
+  end
+
+  it 'reports two level hashes as two lines' do
+    subject = HashDeepDiff::Delta::Right.new(path: :a, value: medium[:a])
+
+    assert_equal("-left[a][b] = b\n-left[a][c] = c", subject.to_s)
+  end
+
+  it 'is"s left diff is always nil' do
+    subject = HashDeepDiff::Delta::Right.new(path: :a, value: small[:a])
+
+    assert_nil(subject.left)
+  end
+
+  it 'is"s right diff is nonempty' do
+    subject = HashDeepDiff::Delta::Right.new(path: :a, value: small[:a])
+
+    assert_equal('b', subject.right)
+  end
 end
