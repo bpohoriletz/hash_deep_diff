ef5e0f6c506f6b2d4cf4924649b68b0661c44672
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-04-18 15:54:37 +0300

    [RFACTOR] Extracted Report class

--- lib/hash_deep_diff/delta.rb
+++ lib/hash_deep_diff/delta.rb
@@ -61,50 +61,5 @@ module HashDeepDiff
 
       Report.new(path: path, value: right)
     end
-
-    # TOFIX poor naming
-    def diff_prefix
-      path.map { |key| "[#{key}]" }.join
-    end
-
-    # Visual representation of the difference between wo values
-    class Report
-      module Mode
-        ADDITION = '+left'
-        DELETION = '-left'
-      end
-
-      def to_str
-        if @value.respond_to?(:to_hash) && !@value.empty?
-          [@mode, diff_prefix, ' = ', "{}\n"].join +
-          @value.keys.map do |key|
-            Report.new(
-              path: @path + [key],
-              value: @value[key],
-              mode: @mode
-            )
-          end.join("\n")
-        else
-          [@mode, diff_prefix, ' = ', @value.to_s].join
-        end
-      end
-
-      def to_s
-        to_str
-      end
-
-      private
-
-      def initialize(path:, value:, mode: Mode::ADDITION)
-        @path = path.to_ary
-        @value = value
-        @mode = mode
-      end
-
-      # TOFIX poor naming
-      def diff_prefix
-        @path.map { |key| "[#{key}]" }.join
-      end
-    end
   end
 end
