86e1c5fbb95b22375dd05f30366aa56e10df4d27
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-04-23 12:42:35 +0300

    [REFACTOR] Etracted Reports::Base from Reports::Diff

---
+++ lib/hash_deep_diff/reports/base.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module HashDeepDiff
+  # Different reporting enjines for {Delta}
+  module Reports
+    # Abstract Class
+    class Base
+      # see {#to_str}
+      # @return [String]
+      def to_s
+        to_str
+      end
+
+      # A report on additions and deletions
+      # @return [String]
+      def to_str
+        original + replacement
+      end
+
+      private
+
+      attr_reader :old_val, :new_val, :path
+
+      # @param [Delta] delta diff to report
+      def initialize(delta:)
+        @path = delta.path.to_ary
+        @old_val = delta.left
+        @new_val = delta.right
+      end
+
+      # old value
+      def original
+        raise AbstractMethodError
+      end
+
+      # new value
+      def replacement
+        raise AbstractMethodError
+      end
+    end
+  end
+end
