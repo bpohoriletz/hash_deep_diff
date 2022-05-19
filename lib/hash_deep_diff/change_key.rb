277014e32aaf167099a4c441c1f730e3f80db176
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-05-19 20:25:33 +0300

    [REFC] Simplified HashDeepDiff::ChangeKey

--- lib/hash_deep_diff/change_key.rb
+++ lib/hash_deep_diff/change_key.rb
@@ -16,21 +16,22 @@ module HashDeepDiff
     # to buld the change representation
     # @return [Array, Hash]
     def self.initial_object(values:)
-      if values.size.positive? && [NESTED_HASH, ARRAY_VALUE].include?(values.first[0].first)
-        []
-      else
-        {}
-      end
+      return [] if values.size.positive? && [NESTED_HASH, ARRAY_VALUE].include?(values[0][0][0])
+
+      {}
     end
 
     # set the value inside Hash based on the change_key
     # @return [Array, Hash]
     # TOFIX; check if @path are mutated
     def set(obj, value, clone_keys = path.clone)
+      # 1. Fetch key
       current_key = clone_keys.shift
-      set_initial_value(current_key, obj, clone_keys)
+      # 2. Prepare object for further processing
+      init_value(current_key, obj, clone_keys)
+      init_nesting(current_key, obj, clone_keys)
+      # 3. Set value - directly or recursively
       set_value(current_key, obj, value, clone_keys)
-      prepare_nesting(current_key, obj, clone_keys)
       recursive_set(current_key, obj, value, clone_keys)
 
       return obj
@@ -68,46 +69,45 @@ module HashDeepDiff
       @path = path.to_ary
     end
 
-    def set_initial_value(current_key, obj, clone_keys)
-      if NESTED_HASH == current_key
-        obj << {} unless obj[-1].respond_to?(:to_hash)
-      elsif [ARRAY_VALUE] == clone_keys
+    # prepare an object before further processing
+    def init_value(current_key, obj, clone_keys)
+      if [ARRAY_VALUE] == clone_keys || NESTED_HASH == clone_keys.first
         obj[current_key] ||= []
-      elsif NESTED_HASH == clone_keys[0]
-        obj[current_key] ||= []
-        obj[current_key] << {} unless obj[current_key][-1].respond_to?(:to_hash)
-      elsif !clone_keys.empty? && ARRAY_VALUE != current_key
+      elsif !clone_keys.empty? && ![ARRAY_VALUE, NESTED_HASH].include?(current_key)
         obj[current_key] ||= {}
       end
     end
 
+    # prepare nesting before further processing
+    def init_nesting(current_key, obj, clone_keys)
+      element = if NESTED_HASH == current_key
+                  obj
+                elsif NESTED_HASH == clone_keys.first
+                  obj[current_key]
+                end
+      element << {} unless element.nil? || element.last.respond_to?(:to_hash)
+    end
+
+    # no more nesting - set value inside object
     def set_value(current_key, obj, value, clone_keys)
       if ARRAY_VALUE == current_key
         obj.prepend(*value)
         clone_keys.pop
-      elsif clone_keys.empty? && obj.respond_to?(:to_hash)
+      elsif clone_keys.empty?
         obj[current_key] = value
         clone_keys.pop
       elsif [ARRAY_VALUE] == clone_keys
-        obj[current_key] ||= []
         obj[current_key] = value + obj[current_key]
         clone_keys.pop
       end
     end
 
-    def prepare_nesting(current_key, obj, clone_keys)
-      if NESTED_HASH == current_key
-        obj << {} unless obj[-1].respond_to?(:to_hash)
-      elsif NESTED_HASH == clone_keys[0]
-        obj[current_key] << {} unless obj[current_key][-1].respond_to?(:to_hash)
-      end
-    end
-
+    # recursion for deeply nested values
     def recursive_set(current_key, obj, value, clone_keys)
       if NESTED_HASH == current_key
-        set(obj[-1], value, clone_keys)
-      elsif NESTED_HASH == clone_keys[0]
-        set(obj[current_key][-1], value, clone_keys[1..])
+        set(obj.last, value, clone_keys)
+      elsif NESTED_HASH == clone_keys.first
+        set(obj[current_key].last, value, clone_keys[1..])
       elsif !clone_keys.empty?
         set(obj[current_key], value, clone_keys)
       end
